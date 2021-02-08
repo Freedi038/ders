b3cf13ab44460799c50cd4da124feb6961553019
Habibi Farid <FaridTH@code.local>
2021-02-08 16:54:40 +0400

    rwar

---
+++ New folder/main.js
@@ -0,0 +1,15 @@
+const qiymet=(n)=>{
+   if(n>60&& n<=100)
+   console.log("a")
+   else if(n>=80)
+   console.log("b")
+   else if(n>=70)
+   console.log("c")
+   else if(n>=60)
+   console.log("d")
+   else if(n>=50)
+   console.log("Kesildin")
+   }
+
+
+qiymet(50)
